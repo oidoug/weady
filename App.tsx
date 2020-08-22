bcfd64e6ea460184895b273a48898e9bafba1f7a
Douglas Schmidt <douglaslondrina@gmail.com>
2020-08-22 16:23:56 -0400

    Added license file

--- App.tsx
+++ App.tsx
@@ -1,14 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import AddCity from './src/screens/AddCity'
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="AddCity" component={AddCity} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +25,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
---
+++ src/screens/AddCity/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const AddCity = () => {
+  return <Text>Add city</Text>
+}
+
+export default AddCity
